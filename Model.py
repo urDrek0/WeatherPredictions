fd6c2ddb63fa24880ef5c7d214dabfd25e53daf0
Bayu <118286302+urDrek0@users.noreply.github.com>
2024-11-27 16:55:47 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,1 +1,13 @@
+import pandas as pd
+
+# Create a dictionary with sample data
+data = {'name': ['Bayu Setyo Prajuritno', 'Muhammad Harits', 'Khoerul Umam', 'Muhammad Affandi Harahap'],
+        'nim': [1103223229, 1103223159, 1103223115, 1103223023]}
+
+# Create a pandas DataFrame
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+df
+
 print("This is ML for machine learning : Weather Predictions")
